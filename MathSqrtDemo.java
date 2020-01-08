bfa774e1f0554959a29a90beae2f7d4e8a08158d
Rajeshkumar Elumalai <38979122+elumalair@users.noreply.github.com>
2020-01-08 21:57:45 +0530

    Create MathSqrtDemo.java

---
+++ MathSqrtDemo.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+
+public class MathSqrtDemo {
+
+   public static void main(String[] args) {
+
+      // get two double numbers numbers
+      double x = 9;
+      double y = 25;
+
+      // print the square root of these doubles
+      System.out.println("Math.sqrt(" + x + ")=" + Math.sqrt(x));
+      System.out.println("Math.sqrt(" + y + ")=" + Math.sqrt(y));
+   }
+}
+
+Let us compile and run the above program, this will produce the following result −
+
+Math.sqrt(9)=3.0
+Math.sqrt(25)=5.0
