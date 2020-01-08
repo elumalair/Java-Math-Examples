8a7c5df89b694b80a6b2f8954c3721e583cab886
Rajeshkumar Elumalai <38979122+elumalair@users.noreply.github.com>
2020-01-08 21:59:06 +0530

    Create JavaMathExample2.java

---
+++ JavaMathExample2.java
@@ -0,0 +1,48 @@
+public class JavaMathExample2    
+    {    
+        public static void main(String[] args)     
+        {    
+            double a = 30;    
+              
+            // converting values to radian    
+            double b = Math.toRadians(a);   
+              
+            // return the trigonometric sine of a      
+            System.out.println("Sine value of a is: " +Math.sin(a));    
+              
+            // return the trigonometric cosine value of a  
+            System.out.println("Cosine value of a is: " +Math.cos(a));  
+              
+            // return the trigonometric tangent value of a  
+            System.out.println("Tangent value of a is: " +Math.tan(a));  
+              
+            // return the trigonometric arc sine of a      
+            System.out.println("Sine value of a is: " +Math.asin(a));    
+              
+            // return the trigonometric arc cosine value of a  
+            System.out.println("Cosine value of a is: " +Math.acos(a));  
+              
+            // return the trigonometric arc tangent value of a  
+            System.out.println("Tangent value of a is: " +Math.atan(a));  
+      
+            // return the hyperbolic sine of a      
+            System.out.println("Sine value of a is: " +Math.sinh(a));    
+              
+            // return the hyperbolic cosine value of a  
+            System.out.println("Cosine value of a is: " +Math.cosh(a));  
+              
+            // return the hyperbolic tangent value of a  
+            System.out.println("Tangent value of a is: " +Math.tanh(a));  
+        }    
+    }   
+    
+    
+Sine value of a is: -0.9880316240928618
+Cosine value of a is: 0.15425144988758405
+Tangent value of a is: -6.405331196646276
+Sine value of a is: NaN
+Cosine value of a is: NaN
+Tangent value of a is: 1.5374753309166493
+Sine value of a is: 5.343237290762231E12
+Cosine value of a is: 5.343237290762231E12
+Tangent value of a is: 1.0
